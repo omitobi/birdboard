b466907e2311b289794e1757311ea4b4dc9978b3
Oluwatobi Samue Omisakin <omisakin.oluwatobi@gmail.com>
2019-01-25 16:30:52 +0200

    Enable user create and see projects

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('projects', [\App\Http\Controllers\ProjectsController::class, 'index']);
+Route::post('projects', [\App\Http\Controllers\ProjectsController::class, 'store']);
+
---
+++ tests/Feature/ProjectsTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ProjectsTest extends TestCase
+{
+
+    use WithFaker, RefreshDatabase;
+
+    public function testUserCanCreateAProject()
+    {
+        $this->withoutExceptionHandling();
+
+        $attributes = [
+            'title' => $this->faker->sentence,
+            'description' => $this->faker->paragraph,
+        ];
+
+        $this->post('/projects', $attributes);
+
+        $this->assertDatabaseHas('projects', $attributes);
+
+        $this->get('/projects')->assertSee($attributes['title']);
+    }
+
+
+}
